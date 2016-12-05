8431a22885a3b700ce4d590737c8915a91cab161
CarlonBaird <carlyestrella@live.com>
2016-12-05 15:09:31 -0400

    Update installserver.sh

--- Sender/installserver.sh
+++ Sender/installserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-workserver_path=/srv/workserver
-mkdir $workserver_path
-cp workserver.py $workserver_path
+sender_path=/srv/sender
+mkdir $sender_path
+cp sender.py $sender_path
 
 # install python3-bottle 
 apt-get -y update
@@ -17,13 +17,13 @@ pip install azure
 
 
 # create a service
-touch /etc/systemd/system/workserver.service
-printf '[Unit]\nDescription=workServer Service\nAfter=rc-local.service\n' >> /etc/systemd/system/workserver.service
-printf '[Service]\nWorkingDirectory=%s\n' $workserver_path >> /etc/systemd/system/workserver.service
-printf 'ExecStart=/usr/bin/python3 %s/workserver.py\n' $workserver_path >> /etc/systemd/system/workserver.service
-printf 'ExecReload=/bin/kill -HUP $MAINPID\nKillMode=process\nRestart=on-failure\n' >> /etc/systemd/system/workserver.service
-printf '[Install]\nWantedBy=multi-user.target\nAlias=workserver.service' >> /etc/systemd/system/workserver.service
+touch /etc/systemd/system/sender.service
+printf '[Unit]\nDescription=workServer Service\nAfter=rc-local.service\n' >> /etc/systemd/system/sender.service
+printf '[Service]\nWorkingDirectory=%s\n' $sender_path >> /etc/systemd/system/sender.service
+printf 'ExecStart=/usr/bin/python3 %s/sender.py\n' $sender_path >> /etc/systemd/system/sender.service
+printf 'ExecReload=/bin/kill -HUP $MAINPID\nKillMode=process\nRestart=on-failure\n' >> /etc/systemd/system/sender.service
+printf '[Install]\nWantedBy=multi-user.target\nAlias=sender.service' >> /etc/systemd/system/sender.service
 
-systemctl start workserver
+systemctl start sender
 
 
